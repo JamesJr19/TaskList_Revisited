52f434286b43a3fec968aaaebec697aaccb7a891
jamesjr19 <ruwartja@gmail.com>
2020-03-01 20:54:41 -0500

    Capstone_TaskList_Revisited Push 1

---
+++ TaskList_Revisited/Homer Simpson _ Simpsons Wiki _ Fandom_files/load(4).php
@@ -0,0 +1,3 @@
+importArticle({type:'script',article:'u:dev:MediaWiki:AjaxRC.js'});;;mw.loader.state({"wikia.fake.articles.d011dadeb30f3ae76b2a73359370aaf0":"ready"});
+
+/* cache key: simpsons:resourceloader:filter:minify-js:7:6a40e1b551e199cc40407876801b557b */
---
+++ TaskList_Revisited/Models/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskList_Revisited.Models
+{
+    public partial class Tasks
+    {
+        public int Id { get; set; }
+       
+        public string TaskName { get; set; }
+        
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+        public string EmployeeId { get; set; }
+
+        public virtual AspNetUsers Employee { get; set; }
+    }
+}
